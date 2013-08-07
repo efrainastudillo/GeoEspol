e1bdcbc6615be674464a702ad51b3b14266c729c
Efrain Astudillo <efrain.astudillo58@gmail.com>
2013-08-07 11:27:53 -0500

    configuracion del settings en la activity principal para importar data a la base de datos

--- src/com/geoespol/activities/MainActivity.java
+++ src/com/geoespol/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -40,5 +41,67 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		
+//		case R.id.quitOption:
+//			log.debug( "quitting app");
+//
+//			finish();
+//
+//			return true;
+//			
+//		case R.id.main_new_project_option:
+//			log.debug( "new project");
+//			Intent npi = new Intent(this, ProjectActivity.class);
+//			npi.putExtra(ProjectActivity.START_MODE, ProjectActivity.START_NEW);
+//			startActivity(npi);
+//			break;
+			
+		case R.id.main_wifi_scan:
+			//log.debug( "starting sample scan activity");
+			//Intent scanIntent = new Intent(this, SampleScanActivity.class);
+			//startActivity(scanIntent);
+			
+			break;
+			
+//		case R.id.main_sensors_option:
+//			log.debug("starting sensors test activity");
+//			Intent sensorsIntent=new Intent(this,SensorsActivity.class);
+//			startActivity(sensorsIntent);
+//			
+//			break;
+			
+			
+		case R.id.main_export_option:
+			//log.debug("starting export db activity");
+			Intent exportIntent=new Intent(this,DBActivity.class);
+			startActivity(exportIntent);
+			
+			
+			break;
+			
+		case R.id.main_settings_option:
+			//log.debug("starting preferences activity");
+			//Intent prefsIntent=new Intent(this,PreferencesActivity.class);
+			//startActivity(prefsIntent);
+			break;
+			
+		case R.id.main_menu_step_calibrate:
+			//log.debug("calibrating sensors");
+			//Intent calibrateIntent=new Intent(this,CalibratorActivity.class);
+			//startActivity(calibrateIntent);
+			break;
+			
+		default:
+			//log.debug("could not identify sender: "+item.getItemId());
+			return super.onOptionsItemSelected(item);
+		}
+		
+		return false;
+
+	}
     
 }
